dabab5b8b0374bf21b8f5cdfa836ead063744d80
cthacker-udel <cthacker@udel.edu>
2021-06-09 08:40:06 -0400

    Added : User API : Get user submission history

--- RedditRestAPI.py
+++ RedditRestAPI.py
@@ -3025,6 +3025,18 @@ def get_user_overview(redditclient):
 
     pprint(request)
 
+def get_user_submitted(redditclient):
+
+    url = base_url + '/user/{}/submitted'.format(redditclient.RedditUser.user)
+
+    headers = get_auth_header(redditclient)
+
+    body = redditclient.RedditUser.generate_queries()
+
+    request = requests.get(url,headers=headers,params=body)
+
+    pprint(request)
+
 
 
 
